f24b014e77d9893dc48bb2f8c6abe24dc86d544f
Sollace <sollacea@gmail.com>
2021-02-07 10:10:08 +0200

    Remove deprecated override

--- src/main/java/com/minelittlepony/client/modmenu/MineLPModMenuFactory.java
+++ src/main/java/com/minelittlepony/client/modmenu/MineLPModMenuFactory.java
@@ -6,12 +6,6 @@ import io.github.prospector.modmenu.api.ModMenuApi;
 import com.minelittlepony.client.GuiPonySettings;
 
 public class MineLPModMenuFactory implements ModMenuApi {
-
-    @Override
-    public String getModId() {
-        return "minelp";
-    }
-
     @Override
     public ConfigScreenFactory<?> getModConfigScreenFactory() {
         return GuiPonySettings::new;
